03c593cdf6b7dc536dfb4e6ed883d1255cfb43f0
ybbyb1 <ybb11.ts@gmail.com>
2025-09-16 10:54:26 +0900

    １段階

---
+++ C/pracrice/total_value_9.rb
@@ -0,0 +1,17 @@
+# https://paiza.jp/works/mondai/prob60/total_value_9
+# 2つの整数の組がn個与えられるので、各組の計算結果を足し合わせたものを出力してください。
+# 各組の計算結果は次の値です。
+# ・2つの整数の組を足し合わせたもの
+# ・ただし、2つの整数が同じ値だった場合は、掛け合わせたもの
+
+n = gets.chomp.to_i
+numbers = n.times.map { gets.chomp.split.map(&:to_i) }
+num = []
+numbers.each do |a, b|
+  if a.eql?(b)
+    num << a * b
+  else
+    num << a + b
+  end
+end
+puts num.sum
